a3b019a6c7ee8205205af05a52ac494b2319d8a8
zpydee <benji@spawntech.io>
2024-01-28 19:32:24 +0200

    docs(storybook): add accessibility addon

--- libs/storybook/.storybook/main.ts
+++ libs/storybook/.storybook/main.ts
@@ -5,7 +5,7 @@ import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../../**/src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
