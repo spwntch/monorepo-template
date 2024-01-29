b2fcc638d6d29a27bd0a506b849c316ea9b59443
zpydee <benji@spawntech.io>
2024-01-29 16:37:15 +0200

    test(react-shared-ui):

---
+++ libs/react/shared/ui/src/lib/components/samples/light/Light.spec.ts
@@ -0,0 +1,12 @@
+
+import { IAttributableImage } from '@spwn/react-shared-ui';
+
+describe('Light', () => {
+  it('should TODO...', () => {
+    const image: IAttributableImage = {
+      src: '/image.png',
+    };
+    // const { baseElement } = render(<Light />);
+    expect(true).toBeTruthy();
+  });
+});
