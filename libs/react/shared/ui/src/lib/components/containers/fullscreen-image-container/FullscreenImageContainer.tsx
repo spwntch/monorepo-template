097016e09e80c45c53403730bd6b84c7ee57af84
zpydee <benji@spawntech.io>
2024-01-31 15:57:40 +0200

    chore(react-shared-ui):

---
+++ libs/react/shared/ui/src/lib/components/containers/fullscreen-image-container/FullscreenImageContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const FullscreenImageContainer = (props: Props) => {
+  return (
+    <div className='w-96 h-72 border-2 border-black'></div>
+  )
+}
+
+export default FullscreenImageContainer
