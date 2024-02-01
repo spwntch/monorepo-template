a7425508d1e4631fd758ef5666c142518195ab1d
zpydee <benji@spawntech.io>
2024-02-01 14:35:03 +0200

    docs(react-shared-ui): add an example of a container with a child

--- libs/react/shared/ui/src/lib/components/containers/background-image-container/BackgroundImageContainer.stories.tsx
+++ libs/react/shared/ui/src/lib/components/containers/background-image-container/BackgroundImageContainer.stories.tsx
@@ -8,11 +8,24 @@ const meta: Meta<typeof BackgroundImageContainer> = {
 export default meta;
 type Story = StoryObj<typeof BackgroundImageContainer>;
 
-export const Simple: Story = {
+export const ContainerOnly: Story = {
   render: () => (
     <div className="h-96 aspect-video">
       <BackgroundImageContainer />
-      
+    </div>
+  ),
+};
+
+export const WithAStyledChild: Story = {
+  render: () => (
+    <div className="h-96 aspect-video">
+      <BackgroundImageContainer>
+        <div className="h-full flex justify-center items-center ">
+          <div className="text-red-500 text-5xl bg-cyan-200 p-4 rounded-2xl">
+            Hello World!
+          </div>
+        </div>
+      </BackgroundImageContainer>
     </div>
   ),
 };
