6b546d0e6ffc6d07269f91c316c0b104ce63f299
zpydee <benji@spawntech.io>
2024-02-01 16:55:20 +0200

    refactor(react-shared-site-shell):

---
+++ libs/react/shared/site-shell/src/lib/components/site-shell/SiteShell.config.ts
@@ -0,0 +1,42 @@
+import {
+    PhoneIcon,
+    PlayCircleIcon,
+    RectangleGroupIcon
+} from '@heroicons/react/20/solid';
+import {
+    ChartPieIcon,
+    CursorArrowRaysIcon,
+    FingerPrintIcon,
+    SquaresPlusIcon
+} from '@heroicons/react/24/outline';
+export const products = [
+  {
+    name: 'Analytics',
+    description: 'Get a better understanding where your traffic is coming from',
+    href: '#',
+    icon: ChartPieIcon,
+  },
+  {
+    name: 'Engagement',
+    description: 'Speak directly to your customers with our engagement tool',
+    href: '#',
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: 'Security',
+    description: 'Your customers’ data will be safe and secure',
+    href: '#',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Integrations',
+    description: 'Your customers’ data will be safe and secure',
+    href: '#',
+    icon: SquaresPlusIcon,
+  },
+];
+export const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+  { name: 'View all products', href: '#', icon: RectangleGroupIcon },
+];
--- libs/react/shared/site-shell/src/lib/components/site-shell/SiteShell.tsx
+++ libs/react/shared/site-shell/src/lib/components/site-shell/SiteShell.tsx
@@ -1,55 +1,51 @@
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-  RectangleGroupIcon,
-} from '@heroicons/react/20/solid';
-import {
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Fragment, useState } from 'react';
-
-const products = [
-  {
-    name: 'Analytics',
-    description: 'Get a better understanding where your traffic is coming from',
-    href: '#',
-    icon: ChartPieIcon,
-  },
-  {
-    name: 'Engagement',
-    description: 'Speak directly to your customers with our engagement tool',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  {
-    name: 'Security',
-    description: 'Your customers’ data will be safe and secure',
-    href: '#',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Integrations',
-    description: 'Your customers’ data will be safe and secure',
-    href: '#',
-    icon: SquaresPlusIcon,
-  },
-];
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-  { name: 'View all products', href: '#', icon: RectangleGroupIcon },
-];
+import { callsToAction, products } from './SiteShell.config';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
+
+const Logo = () => {
+  return (
+    <a href="#" className="-m-1.5 p-1.5">
+      <span className="sr-only">Your Company</span>
+      <img
+        className="h-8 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt=""
+      />
+    </a>
+  );
+};
+
+type MobileMenuButtonProps = {
+  onClick: () => void;
+};
+
+const OpenMobileMenuButton = ({ onClick }: MobileMenuButtonProps) => (
+  <button
+    type="button"
+    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+    onClick={onClick}
+  >
+    <span className="sr-only">Open main menu</span>
+    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+  </button>
+);
+
+const CloseMobileMenuButton = ({ onClick }: MobileMenuButtonProps) => (
+  <button
+    type="button"
+    className="-m-2.5 rounded-md p-2.5 text-gray-700"
+    onClick={() => onClick()}
+  >
+    <span className="sr-only">Close menu</span>
+    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+  </button>
+);
 
 export default function SiteShell() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -61,24 +57,10 @@ export default function SiteShell() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
-          </a>
+          <Logo />
         </div>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
+          <OpenMobileMenuButton onClick={() => setMobileMenuOpen(true)} />
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover>
@@ -171,22 +153,8 @@ export default function SiteShell() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
+            <Logo />
+            <CloseMobileMenuButton onClick={() => setMobileMenuOpen(false)} />
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
